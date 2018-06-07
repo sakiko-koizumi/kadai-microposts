e2c98a25c61f4be75dbb228a84983e7b758f1971
sakiko.koizumi <sakiko.koizumi@rakuten.com>
2018-06-07 00:56:53 +0000

    timeline

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -19,9 +19,11 @@ class MicropostsController extends Controller
                 'user' => $user,
                 'microposts' => $microposts,
             ];
+            $data += $this->counts($user);
+            return view('users.show', $data);
+        }else {
+            return view('welcome');
         }
-            return view('welcome',$data);
-        
     }
     
        public function store(Request $request)
